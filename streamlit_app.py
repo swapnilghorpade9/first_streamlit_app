09d1103de84839a2f6e53a0cb7a17efe334f5004
swapnilghorpade9 <139347832+swapnilghorpade9@users.noreply.github.com>
2023-07-12 07:29:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('This is my first Git and Streamlit')
+streamlit.header('Breakfast Menu')
+streamlit.text('test1 and test2')
+streamlit.text('test3 and test4')
+streamlit.text('test5 and test6')
