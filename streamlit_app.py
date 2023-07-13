4ed8bb6b68dbcc376776be846c93697c17a4b4dd
swapnilghorpade9 <139347832+swapnilghorpade9@users.noreply.github.com>
2023-07-13 06:53:05 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,12 +26,20 @@ try:
   if not fruit_choice:
     streamlit.error("please select a fruit to get information")
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    # streamlit.text(fruityvice_response.json())
-    # write your own comment -what does the next line do? 
+    # create repeatable code block(called a function)
+    def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    # write your own comment - what does this do?
-    streamlit.dataframe(fruityvice_normalized)
+    return fruityvice_normalized
+
+streamlit.header("Fruityvice Fruit Advice!")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("please select a fruit to get information")
+  else:
+    back_from_function = get_fruityvice_data (fruit_choice)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 
